4593b6b7378733da90689168ecf323a3e5d32cd0
Dnyaneshwar Bhalerao <dnyaneshwar.bhalerao@synerzip.com>
2018-02-15 11:39:49 +0530

    Removed unnecessary files

--- src/calendar/config/calendarConfig.js
+++
@@ -1,6 +0,0 @@
-export let months = [" January ", " February", " March ", " April ", " May ",
-  " June ", " July ", " August ", " September ", " October ", " November",
-  " December "];
-
-export let weekDays = [" Mon ", " Tue ", " Wed ", " Thu", " Fri ", " Sat ",
- " Sun "];
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Calendar from './calendar/calendarView';
-
-ReactDOM.render(
-    <Calendar/>,
-	document.getElementById('app'));
